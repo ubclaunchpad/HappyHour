62020946ac4b8191bea837329208d6d048399f32
Francis Macapobre <f.macapobre@gmail.com>
2020-10-15 13:41:48 -0700

    hello world

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to when3meet"/>
   </div>
 </template>
 
