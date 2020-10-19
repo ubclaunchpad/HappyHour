f9c3bf102ed407e566a5f43bf31c317954b05acd
gokcedilek <gokcedilek99@gmail.com>
2020-10-18 20:53:18 -0700

    change log package and add comments

--- backend/test_firestore.go
+++ backend/test_firestore.go
@@ -3,12 +3,13 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"google.golang.org/api/option"
 	"firebase.google.com/go"
 )
 
+// example document definition
 type Data struct {
 	Key string
 	Value string
@@ -18,25 +19,27 @@ func AddData(w http.ResponseWriter, r *http.Request) {
 	// return json
 	w.Header().Set("Content-type", "application/json")
 
-	//initialize the admin sdk
-	sa := option.WithCredentialsFile("." +
-		"/ServiceAccountKey.json")
+	// initialize the admin sdk
+	sa := option.WithCredentialsFile("./ServiceAccountKey.json")
 	app, err := firebase.NewApp(context.Background(),
 		nil, sa)
 
 	client, err := app.Firestore(context.Background())
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
 
 	defer client.Close()
 
-	//save random data to firestore -- TODO!
+	// save an example document to firestore
 	var data Data
+
+	// unmarshal the JSON in the request to the data struct
+	// to access the key-value pairs
 	err = json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		log.Fatalln("failed to decode the request: %v", err)
+		log.Fatal("failed to decode the request: %v", err)
 	}
 
 	_, _ ,err = client.Collection("test").Add(context.
@@ -45,10 +48,10 @@ func AddData(w http.ResponseWriter, r *http.Request) {
 		"value": data.Value,
 	})
 	if err != nil {
-		log.Fatalln("failed to add data : %v", err)
+		log.Fatal("failed to add data : %v", err)
 	}
 
-	log.Println("request body: %v", data)
 	w.WriteHeader(http.StatusOK)
+	// marshal the data struct to JSON to send as response
 	json.NewEncoder(w).Encode(data)
 }
