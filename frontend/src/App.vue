febfba9f675edf16a788a5fcc953dc56f31e402d
Nanda Syahrasyad <nanda.s@hey.com>
2020-10-24 19:11:37 -0700

    feat: upgrade to Vue 3 (#18)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to when3meet" />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Counter />
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import Counter from "./components/Counter.vue";
 
-export default Vue.extend({
-  name: 'App',
+export default defineComponent({
+  name: "App",
   components: {
     HelloWorld,
-  },
-})
+    Counter
+  }
+});
 </script>
 
 <style>
