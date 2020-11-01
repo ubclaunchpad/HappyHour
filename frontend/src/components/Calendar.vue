9ce93831cb186631b0261b5015e9f8de534614ee
xanhlaok <xanhlaok@users.noreply.github.com>
2020-10-31 19:12:40 -0700

    Setup boilerplate for VueCal

---
+++ frontend/src/components/Calendar.vue
@@ -0,0 +1,24 @@
+<template>
+  <vue-cal
+    today-button
+    :time-step="30"
+    :disable-views="['years']"
+    :snap-to-time="15"
+    editable-events
+    :events="events"
+    class="vuecal--full-height-delete"
+  >
+  </vue-cal>
+</template>
+
+<script>
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+
+export default {
+  components: { VueCal },
+  data: () => ({})
+};
+</script>
+
+<style></style>
