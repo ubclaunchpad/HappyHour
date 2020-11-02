b3f49751bf32113dc275939ec5cc0d1ef6c75d38
Narendra Syahrasyad <nanda.s@hey.com>
2020-11-02 14:14:41 -0800

    feat: add typescript types

---
+++ frontend/src/types.ts
@@ -0,0 +1,30 @@
+/**
+ * Types as per Week 4 meeting:
+ *  - https://www.notion.so/Team-4-New-Wireframes-c3e68fecf847456a8ca91c7d1514fb38
+ */
+
+export interface Block {
+  startTime: Date;
+  availableUsers: string[];
+}
+
+export interface Calendar {
+  blocks: Block[];
+}
+
+export interface User {
+  username: string;
+  email: string;
+  calendar: Calendar;
+}
+
+export interface Event {
+  users: string[];
+  owners: string[];
+  scheduleWindow: {
+    startTime: Date;
+    endTime: Date;
+  };
+  link: string;
+  calendar: Calendar;
+}
