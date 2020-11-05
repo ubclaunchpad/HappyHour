2e8efa03e3028de02215f762d42c4ce3910dcadf
Nanda Syahrasyad <nanda.s@hey.com>
2020-11-04 22:09:48 -0800

    #22 - feat: add typescript types (#25)

---
+++ frontend/src/components/Calendar.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="calendar">
+    <div class="times">
+      <header>Times</header>
+      <div class="time" v-for="time in timeList" :key="`calendar-${time}`">
+        {{ time }}
+      </div>
+    </div>
+    <div class="wrapper">
+      <CalendarDay
+        v-for="{ date, blocks } in days"
+        :key="date"
+        :date="date"
+        :times="times"
+        :blocks="blocks"
+        @update:blocks="updateCalendar"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { eachDayOfInterval, getHours, isSameDay } from "date-fns";
+import { defineComponent, PropType } from "vue";
+
+import { Block, Calendar, Time } from "@/types";
+import { formatHour } from "@/utils/time";
+import CalendarDay from "./CalendarDay.vue";
+
+interface Day {
+  date: string;
+  blocks: Block[];
+}
+
+const Interval = 30; // minutes
+const MinsInHour = 60;
+
+export default defineComponent({
+  components: {
+    CalendarDay
+  },
+  props: {
+    calendar: {
+      type: Object as PropType<Calendar>,
+      required: true
+    },
+    startTime: {
+      type: String,
+      required: true
+    },
+    endTime: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    blocks(): Block[] {
+      return this.calendar.blocks;
+    },
+    days(): Day[] {
+      return eachDayOfInterval({
+        start: this.start,
+        end: this.end
+      }).map(date => {
+        return {
+          date: date.toISOString(),
+          blocks: this.blocks.filter(block => isSameDay(block.startTime, date))
+        };
+      });
+    },
+    earliestTime(): number {
+      return getHours(this.start);
+    },
+    end(): Date {
+      return new Date(this.endTime);
+    },
+    latestTime(): number {
+      return getHours(this.end);
+    },
+    start(): Date {
+      return new Date(this.startTime);
+    },
+    times(): Time[] {
+      const hours = [];
+      for (let hour = this.earliestTime; hour <= this.latestTime; hour++) {
+        for (let i = 0; i < MinsInHour / Interval; i++) {
+          hours.push({ hour, minutes: i * Interval });
+        }
+      }
+      return hours.slice(0, hours.length - 1);
+    },
+    timeList(): string[] {
+      return this.times.map(time => formatHour(time.hour, time.minutes));
+    }
+  },
+  methods: {
+    updateCalendar({ blocks, date }: { blocks: Block[]; date: string }) {
+      const originalBlocks = this.days.find(day => day.date === date)?.blocks;
+      if (originalBlocks) {
+        const otherBlocks = this.blocks.filter(
+          block => !originalBlocks.includes(block)
+        );
+        this.$emit("update:calendar", { blocks: [...otherBlocks, ...blocks] });
+      }
+    }
+  }
+});
+</script>
+
+<style scoped>
+.calendar {
+  display: flex;
+  padding-left: 4rem;
+  padding-right: 4rem;
+  font-size: 12px;
+}
+
+.times {
+  flex: 1;
+  text-align: right;
+  margin-right: 1rem;
+}
+
+.time {
+  height: 2em;
+  border: 0.5px solid transparent;
+}
+
+.wrapper {
+  display: flex;
+  flex: 12;
+  justify-content: space-between;
+}
+</style>
---
+++ frontend/src/components/CalendarDay.vue
@@ -0,0 +1,118 @@
+<template>
+  <section class="day">
+    <header>{{ dateText }}</header>
+    <div
+      v-for="time in times"
+      :class="['block', { selected: isActive(time) }]"
+      :key="`${dateText}-${time}`"
+      @touchstart="handleMouseDown(time, $event)"
+      @mousedown="handleMouseDown(time, $event)"
+      @touchmove="handleMouseOver(time, $event)"
+      @mouseover="handleMouseOver(time, $event)"
+      @touchend="handleMouseUp"
+      @mouseup="handleMouseUp"
+    ></div>
+  </section>
+</template>
+
+<script lang="ts">
+import { format, parse } from "date-fns";
+import { defineComponent, PropType } from "vue";
+
+import { Block, Time } from "@/types";
+import { toTime } from "@/utils/time";
+
+export default defineComponent({
+  props: {
+    date: {
+      type: String,
+      required: true
+    },
+    times: {
+      type: Object as PropType<Array<Time>>,
+      required: true
+    },
+    blocks: {
+      type: Object as PropType<Array<Block>>,
+      required: true
+    }
+  },
+  data() {
+    return {
+      dragging: false
+    };
+  },
+  computed: {
+    dateText(): string {
+      return format(new Date(this.date), "E MMM d");
+    }
+  },
+  mounted() {
+    window.addEventListener("touchend", this.handleMouseUp);
+    window.addEventListener("mouseup", this.handleMouseUp);
+  },
+  beforeUnmount() {
+    window.removeEventListener("touchend", this.handleMouseUp);
+    window.removeEventListener("mouseup", this.handleMouseUp);
+  },
+  methods: {
+    handleMouseOver(time: Time, evt: TouchEvent) {
+      if (this.dragging) {
+        evt.preventDefault();
+        this.toggle(time);
+      }
+    },
+    handleMouseDown(time: Time, evt: TouchEvent) {
+      evt.preventDefault();
+      this.dragging = true;
+      this.toggle(time);
+    },
+    handleMouseUp(evt: TouchEvent | MouseEvent) {
+      evt.preventDefault();
+      this.dragging = false;
+    },
+    isActive(time: Time) {
+      const index = this.blocks.findIndex(block =>
+        this.equalsBlock(time, block)
+      );
+      return index >= 0;
+    },
+    toggle(time: Time) {
+      let blocks: Block[] = [];
+      if (this.isActive(time)) {
+        blocks = this.blocks.filter(block => !this.equalsBlock(time, block));
+      } else {
+        const newBlock: Block = {
+          startTime: parse(
+            `${this.dateText} ${time.hour}:${time.minutes}`,
+            "E MMM d H:mm",
+            new Date()
+          ),
+          availableUsers: ["1"] // stub
+        };
+        blocks = [...this.blocks, newBlock];
+      }
+      this.$emit("update:blocks", { blocks, date: this.date });
+    },
+    equalsBlock(time: Time, block: Block) {
+      const blockTime = toTime(block.startTime);
+      return blockTime.hour === time.hour && blockTime.minutes === time.minutes;
+    }
+  }
+});
+</script>
+
+<style scoped>
+.day {
+  flex: 1;
+}
+
+.block {
+  border: 0.5px solid lightgray;
+  height: 2em;
+}
+
+.selected {
+  background: green;
+}
+</style>
