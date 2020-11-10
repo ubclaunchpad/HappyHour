10068a74124525698feb6f204f852e1a8fb3ad0c
xanhlaok <xanhlaok@users.noreply.github.com>
2020-11-10 15:48:13 -0800

    adjusted styling to match design

--- frontend/src/components/EventRespondents.vue
+++ frontend/src/components/EventRespondents.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <p class="label">Respondents</p>
+    <div class="sub-heading">
+      <h3>Respondents ({{ respondents.length }})</h3>
+    </div>
     <div class="respondents">
-      <ul class="respondent">
-        <li v-for="respondent in respondents" :key="respondent">
+      <ul>
+        <li
+          class="respondent"
+          v-for="respondent in respondents"
+          :key="respondent"
+        >
           {{ respondent }}
         </li>
       </ul>
@@ -56,45 +62,22 @@ export default defineComponent({
 .respondents {
   border-radius: 5px;
   background: rgb(255, 255, 255);
-  padding: 20px;
-  width: 40%;
-  height: 50ch;
-  margin: auto;
-  margin-bottom: 20px;
+  width: 230px;
+  height: 405px;
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  transition: 0.3s;
-}
+  padding: 1rem 1.125rem;
 
-.respondents:hover {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  overflow-y: scroll;
+  text-align: left;
+
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: 0.12px;
 }
 
 .respondent {
   list-style-type: none;
-  margin: 0;
-  padding: 0;
-  height: 50ch;
-
-  font-style: normal;
-  font-weight: 500;
-  font-size: 15px;
-  line-height: 25px;
-
-  align-items: center;
-  letter-spacing: -0.56px;
-
-  overflow: hidden;
-  overflow-y: scroll;
-}
-
-.label {
-  font-style: normal;
-  font-weight: 500;
-  font-size: 24px;
-  line-height: 44px;
-
-  align-items: center;
-  letter-spacing: -0.56px;
-  filter: drop-shadow(0px 4px 5px rgba(0, 0, 0, 0.25));
+  margin-bottom: 12px;
 }
 </style>
