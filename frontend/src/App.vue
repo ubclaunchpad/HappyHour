663af5f1e31a5fd70f889282aba616048ce28f41
xanhlaok <xanhlaok@users.noreply.github.com>
2020-11-10 15:45:54 -0800

    added container and global styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <TheNavbar />
-  <router-view />
-  <SidebarCollapsed />
+  <div class="container-page">
+    <TheNavbar />
+    <router-view />
+    <SidebarCollapsed />
+  </div>
 </template>
 
 <script lang="ts">
@@ -18,6 +20,7 @@ export default defineComponent({
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap");
+
 #app {
   font-family: "IBM Plex Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -32,5 +35,32 @@ export default defineComponent({
   padding: 0;
   border: 0;
   box-sizing: border-box;
+}
+
+/* Global Class Styling */
+.container-page {
+  height: 100%;
+  width: 100%;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  overflow: hidden;
+  position: absolute;
+  background: RGB(229, 229, 229);
+  z-index: -2;
+}
+
+.heading {
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 44px;
+}
+
+.sub-heading {
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 44px;
+  text-align: left;
 }
 </style>
