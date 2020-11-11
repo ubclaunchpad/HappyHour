1646d9e811a99e03729ccc41d3df90ff9c3391fc
xanhlaok <xanhlaok@users.noreply.github.com>
2020-11-11 02:15:44 -0800

    adjusted global styling and css resets

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,14 +21,6 @@ export default defineComponent({
 <style>
 @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap");
 
-#app {
-  font-family: "IBM Plex Sans", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: rgb(48, 48, 48);
-}
-
 /* Reset CSS */
 * {
   margin: 0;
@@ -37,7 +29,20 @@ export default defineComponent({
   box-sizing: border-box;
 }
 
+ol,
+ul {
+  list-style: none;
+}
+
 /* Global Class Styling */
+#app {
+  font-family: "IBM Plex Sans", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgb(48, 48, 48);
+}
+
 .container-page {
   height: 100%;
   width: 100%;
@@ -46,21 +51,28 @@ export default defineComponent({
   right: 0;
   left: 0;
   overflow: hidden;
+  overflow-y: auto;
   position: absolute;
   background: RGB(229, 229, 229);
   z-index: -2;
 }
 
 .heading {
+  font-style: normal;
   font-weight: bold;
   font-size: 24px;
   line-height: 44px;
+  letter-spacing: -0.56px;
+  color: RGB(48, 48, 48);
 }
 
 .sub-heading {
+  font-style: normal;
   font-weight: 500;
   font-size: 24px;
   line-height: 44px;
+  letter-spacing: -0.56px;
+  color: RGB(48, 48, 48);
   text-align: left;
 }
 </style>
