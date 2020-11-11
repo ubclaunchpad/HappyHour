c0ebbde1fc9da8ba9906c9f51a72de1418cc40bb
xanhlaok <xanhlaok@users.noreply.github.com>
2020-11-11 02:17:53 -0800

    adjusted styling & added todos

--- frontend/src/components/EventRespondents.vue
+++ frontend/src/components/EventRespondents.vue
@@ -3,23 +3,19 @@
     <div class="sub-heading">
       <h3>Respondents ({{ respondents.length }})</h3>
     </div>
-    <div class="respondents">
-      <ul>
-        <li
-          class="respondent"
-          v-for="respondent in respondents"
-          :key="respondent"
-        >
-          {{ respondent }}
-        </li>
-      </ul>
-    </div>
+
+    <ul class="respondents">
+      <li class="resp-item" v-for="(resp, index) in respondents" :key="index">
+        {{ resp }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+// FIXME: Pass in prop via EventRespondents respondents="[...]"
 export default defineComponent({
   props: {
     respondents: {
@@ -54,32 +50,28 @@ export default defineComponent({
         "?"
       ]
     }
-  },
-
-  components: {}
+  }
 });
 </script>
 
 <style scoped>
+/* TODO: Reconsider relative units */
 .respondents {
   border-radius: 5px;
-  background: rgb(255, 255, 255);
-  width: 230px;
-  height: 300px;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  background: RGB(255, 255, 255);
+  width: 25ch;
+  height: 30ch;
+  filter: drop-shadow(0px 4px 4px RGBA(0, 0, 0, 0.25));
   padding: 1rem 1.125rem;
-
   overflow: hidden;
-  overflow-y: scroll;
+  overflow-y: auto;
   text-align: left;
-
   font-weight: 500;
   line-height: 24px;
   letter-spacing: 0.12px;
 }
 
-.respondent {
-  list-style-type: none;
+.resp-item {
   margin-bottom: 12px;
 }
 </style>
