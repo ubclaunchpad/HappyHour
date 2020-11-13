e978af6076a03913ceac426225427bc5de98ae4c
Jill Bao <jbao@fispan.com>
2020-11-12 23:22:00 -0800

    created login component

---
+++ frontend/src/components/LoginPage.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="login">
+    <div class="heading">Log In</div>
+    <div class="p1">
+      <p>Username/Email: {{ username }}</p>
+      <input class="input" v-model="username" />
+      <p>Password: {{ password }}</p>
+      <input class="input" v-model="password" />
+    </div>
+    <div class="button">
+      <AppButton @update="logIn()" text="Log in" />
+    </div>
+    <div class="button">
+      <AppButton @update="logInViaGoogle()" text="Log in with Google Account" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppButton from "@/components/AppButton.vue";
+
+export default defineComponent({
+  components: {
+    AppButton
+  },
+
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+
+  methods: {
+    logIn() {
+      // log in
+    },
+    logInViaGoogle() {
+      // log in via Google Account
+    }
+  }
+});
+</script>
+
+<style scoped>
+.heading {
+  font-weight: 500;
+}
+.input {
+  width: 422px;
+  height: 38px;
+
+  background: #ffffff;
+  border: 1px solid #4760f3;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+  margin-bottom: 20px;
+}
+.p1 {
+  text-align: left;
+}
+.button {
+  width: 422px;
+  padding-top: 10px;
+  margin-bottom: 10px;
+}
+.login {
+  position: absolute;
+  width: 556px;
+  height: 484px;
+  left: 442px;
+  top: 234px;
+
+  background: #ffffff;
+  border-radius: 8px;
+  padding: 55px;
+}
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login"><LoginPage /></div>
+</template>
+
+<script>
+import LoginPage from "@/components/LoginPage.vue";
+
+export default {
+  components: {
+    LoginPage
+  }
+};
+</script>
+
+<style>
+.login {
+  align-items: center;
+  align-content: center;
+}
+</style>
