49e1d6b46f4f8b9226d6cd3ce7b54b3074074252
Jill Bao <52767104+baojill@users.noreply.github.com>
2020-11-14 00:33:35 -0800

    created login page (#47)
    Co-authored-by: Jill Bao <jbao@fispan.com>

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login"><LoginPage /></div>
+</template>
+
+<script>
+import LoginPage from "@/components/LoginPage.vue";
+export default {
+  components: {
+    LoginPage
+  }
+};
+</script>
+
+<style>
+.login {
+  align-items: center;
+  align-content: center;
+}
+</style>
