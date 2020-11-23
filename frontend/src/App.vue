dd00534c6d48ff6efc6c287a134fa7a192ca9e4b
xanhlaok <xanhlaok@users.noreply.github.com>
2020-11-22 21:57:54 -0800

    added Normalizer.css and global styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-page">
+  <div class="root-container">
     <TheNavbar />
     <router-view />
   </div>
@@ -18,14 +18,15 @@ export default defineComponent({
 </script>
 
 <style>
+@import "~normalize.css";
 @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap");
-
-/* Reset CSS */
+/* Reset CSS in addition to Normalize.css*/
 * {
   margin: 0;
   padding: 0;
   border: 0;
   box-sizing: border-box;
+  font-weight: normal;
 }
 
 ol,
@@ -35,14 +36,14 @@ ul {
 
 /* Global Class Styling */
 #app {
-  font-family: "IBM Plex Sans", sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: rgb(48, 48, 48);
 }
 
-.container-page {
+.root-container {
   height: 100%;
   width: 100%;
   top: 0;
@@ -55,7 +56,7 @@ ul {
   background: RGB(229, 229, 229);
   z-index: -2;
 }
-
+/*
 .heading {
   font-style: normal;
   font-weight: bold;
@@ -64,6 +65,7 @@ ul {
   letter-spacing: -0.56px;
   color: RGB(48, 48, 48);
 }
+*/
 
 .sub-heading {
   font-style: normal;
@@ -73,5 +75,40 @@ ul {
   letter-spacing: -0.56px;
   color: RGB(48, 48, 48);
   text-align: left;
+}
+
+/* Headline 1 */
+h1 {
+  font-size: 5.9375rem; /*95px on default 16px*/
+}
+
+/* Headline 2 */
+h2 {
+  font-size: 3.6875rem; /*59px*/
+}
+
+/* Headline 3 */
+h3 {
+  font-size: 3rem; /*48px*/
+}
+
+/* Headline 4 */
+h4 {
+  font-size: 2.125rem; /*34px*/
+}
+
+/* Headline 5 */
+h5 {
+  font-size: 1.5rem; /*24px*/
+}
+
+/* Headline 6 */
+h6 {
+  font-size: 1.25rem; /*20px*/
+}
+
+/* Body1 */
+body {
+  font-size: 1rem; /*16px*/
 }
 </style>
