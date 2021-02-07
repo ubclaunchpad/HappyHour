451dd835e190a19e11ddfea1bfce12a9b75493e3
Jill Bao <jbao@fispan.com>
2021-02-06 16:59:27 -0800

    created appToggle component with new API

--- src/common/AppToggle.vue
+++ src/common/AppToggle.vue
@@ -1,23 +1,21 @@
 <template>
-  <label
-    class="container"
-    :style="'--switch-container-width: 20rem'"
-    @click="update"
-  >
+  <label class="container">
     <input
       v-bind="$attrs"
       class="input"
       type="checkbox"
-      :checked="checked"
-      @change="$emit('update:checked', $event.target.checked)"
+      :checked="modelValue"
+      @change="$emit('update:modelValue', $event.target.checked)"
     />
-    <span class="switch"></span>
-    <span :class="{ leftEnabled: !checked, leftDisabled: checked }">{{
-      leftText
-    }}</span>
-    <span :class="{ rightDisabled: !checked, rightEnabled: checked }">{{
-      rightText
-    }}</span>
+
+    <span class="switch">
+      <span :class="{ leftEnabled: !modelValue, leftDisabled: modelValue }">{{
+        leftText
+      }}</span>
+      <span :class="{ rightDisabled: !modelValue, rightEnabled: modelValue }">{{
+        rightText
+      }}</span>
+    </span>
   </label>
 </template>
 
@@ -33,12 +31,12 @@ export default {
       type: String,
       required: true
     },
-    checked: {
+    modelValue: {
       type: Boolean,
       required: true
     }
   },
-  emits: ["update:checked", "update"],
+  emits: ["update:checked"],
   methods: {
     update() {
       this.$emit("update");
@@ -49,49 +47,60 @@ export default {
 
 <style scoped>
 .container {
+  /* width variable */
+  --switch-container-width: 20rem;
+  /* dimensions */
   width: var(--switch-container-width);
   cursor: pointer;
   display: flex;
   align-items: center;
 }
+
 .leftDisabled {
   flex-shrink: 0;
-  margin-left: calc(var(--switch-container-width) * -1 + 2rem);
-  color: #c4c4c4;
+  margin-left: calc(var(--switch-container-width) * -1 / 2 + 2rem);
+  color: #a0aec0;
+  z-index: 1;
 }
+
 .leftEnabled {
   flex-shrink: 0;
-  margin-left: calc(var(--switch-container-width) * -1 + 2rem);
+  margin-left: calc(var(--switch-container-width) * -1 / 2 + 2rem);
   color: #020f22;
+  z-index: 1;
 }
+
 .rightDisabled {
   flex-shrink: 0;
-  margin-left: 3rem;
-  color: #c4c4c4;
+  margin-left: 5rem;
+  color: #a0aec0;
+  z-index: 1;
 }
+
 .rightEnabled {
   flex-shrink: 0;
-  margin-left: 3rem;
+  margin-left: 5rem;
   color: #020f22;
+  z-index: 1;
 }
+
 /* Visually hide the checkbox input */
 .input {
-  /* position: relative; */
-  width: 1px;
-  height: 1px;
+  width: 0;
+  height: 0;
   padding: 0;
   margin: -1px;
   overflow: hidden;
-  clip: rect(0, 0, 0, 0);
   white-space: nowrap;
   border-width: 0;
 }
+
 .switch {
+  /* colour variables */
   --switch-container-size: 80px;
   --switch-size: calc(var(--switch-container-size) / 2);
-  --blue: #4760f3;
-  --dark-blue: #3a4fca;
-  --white: #fff;
+  --gray: #cbd5e0;
+  --dark-gray: #a0aec0;
   --accent: #375786;
   /* Vertically center the inner circle */
   display: flex;
@@ -99,14 +108,14 @@ export default {
   position: relative;
   height: var(--switch-size);
   flex-basis: var(--switch-container-width);
-  z-index: -1;
   /* Make the container element rounded */
   border-radius: var(--switch-size);
+  border: 2px solid var(--dark-gray);
   background-color: white;
   /* In case the label gets really long, the toggle shouldn't shrink. */
   flex-shrink: 0;
-  transition: background-color 0.25s ease-in-out;
 }
+
 .switch::before {
   content: "";
   /* position: absolute; */
@@ -114,10 +123,11 @@ export default {
   width: calc(var(--switch-container-width) / 2);
   /* Make the inner circle fully rounded */
   border-radius: var(--switch-size);
-  background-color: var(--white);
+  background-color: white;
   border: 2px solid var(--accent);
   transition: transform 0.375s ease-in-out;
 }
+
 .input:checked + .switch::before {
   /* Move the inner circle to the right */
   transform: translateX(calc(var(--switch-container-width) / 2));
