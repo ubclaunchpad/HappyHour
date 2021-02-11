5db7637ea55e681eba8be6bd67cfdbf631bbff98
James Meng <35415298+jmengo@users.noreply.github.com>
2021-02-10 21:28:02 -0800

    feat: add user Auth / CRUD operations (#68)

--- src/user/client.ts
+++ src/user/client.ts
@@ -1,18 +1,70 @@
-import "firebase/auth";
-import { db } from "@/db";
+import { app, db } from "@/db";
 import { Calendar } from "@/calendar/client";
+
+const Auth = app.auth();
 
 export interface User {
   username: string;
   email: string;
-  calendar: Calendar;
+  // calendar: Calendar;
 }
 
 // TODO: Fill this in with methods
 const client = {
-  // stub
+  createUser(email: string, password: string) {
+    try {
+      const user = Auth.createUserWithEmailAndPassword(email, password).then(
+        user => {
+          db.ref("users/" + user.user?.uid).set({
+            username: "placeholder",
+            email: email
+          });
+        }
+      );
+      console.log("OK - Token: " + user);
+    } catch (err) {
+      console.error("ERR: " + err);
+    }
+  },
+  login(email: string, password: string) {
+    return Auth.signInWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log(user?.uid);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  logout() {
+    if (Auth.currentUser) {
+      return Auth.signOut()
+        .then(() => {
+          console.log("Succesfully signed out");
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } else {
+      console.log("No user signed in");
+    }
+  },
+  deleteUser() {
+    if (Auth.currentUser) {
+      return Auth.currentUser
+        .delete()
+        .then(() => console.log("User succesfully deleted"))
+        .catch(() => console.log("There was a problem deleting the user"));
+    } else {
+      console.log("No user signed in");
+    }
+  },
+  updateUser(email: string) {
+    db.ref("users/" + Auth.currentUser?.uid).set({
+      username: "placeholder",
+      email: email
+    });
+  }
 };
 
 export default client;
-
-export const Auth = db.auth();
