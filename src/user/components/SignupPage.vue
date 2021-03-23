15110e2db2931624f8ad163a15d2234f259ab27c
Michelle Yeojin Kim <33190160+michelleykim@users.noreply.github.com>
2021-03-23 15:23:47 -0600

    #65: update app button component (#73)
    * primary and secondary button implemented. Made changes to create.vue, event.vue, loginPage.vue with appropriate type of button
    
    * made the primary button default
    
    * change appbutton to take content with any type, default null
    
    * button takes in html to display as its content
    
    * update event declared on emits option
    
    * add Google icon with 2 variant support
    
    * update AppButton to support variant and logo
    
    * update all instances of AppButton and its styling
    
    * update AppButton instances to include type attribute
    
    Co-authored-by: tnwin <24283779+tnwin@users.noreply.github.com>

--- src/user/components/SignupPage.vue
+++ src/user/components/SignupPage.vue
@@ -8,7 +8,14 @@
       <input v-model="password" class="input" />
     </div>
     <div class="button">
-      <AppButton text="Sign up" @update="signUp()" />
+      <AppButton :class="'btn-primary'" content="Log in" @update="logIn()" />
+    </div>
+    <div class="button">
+      <AppButton
+        :class="'btn-secondary'"
+        content="Log in with Google Account"
+        @update="logInViaGoogle()"
+      />
     </div>
   </div>
 </template>
