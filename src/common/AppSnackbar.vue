c9cf6f4ca36d24d9beb34b8a1d1a357fe913b40d
tnwin <24283779+tnwin@users.noreply.github.com>
2021-04-10 05:43:50 -0700

    refactor: copy styling and code from `AppButton`

--- src/common/AppSnackbar.vue
+++ src/common/AppSnackbar.vue
@@ -1,88 +1,87 @@
-<!--Notification component shows the notification text
-Notification can be shown by usign v-if-->
 <template>
-  <div class="snackbar">
-    <p class="msg">{{ text }}</p>
-    <!--<i class="close" @click="update"></i>-->
-    <svg
-      class="close"
-      width="14"
-      height="14"
-      viewBox="0 0 14 14"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      @click="update"
-    >
-      <path
-        d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z"
-        fill="white"
-      />
-    </svg>
+  <div :variant="variant" :class="currVariant" class="snackbar">
+    <p class="text">{{ text }}</p>
+
+    <button class="btn btn--close" @click="$emit('update')">
+      <svg
+        width="14"
+        viewBox="0 0 14 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z"
+          fill="var(--color-card)"
+        />
+      </svg>
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
+
 export default defineComponent({
   name: "AppSnackbar",
+
   props: {
-    text: {
-      type: String,
-      required: true
-    }
+    variant: { type: String, required: false, default: "primary" },
+    text: { type: String, required: true }
   },
+
   emits: ["update"],
-  methods: {
-    update() {
-      this.$emit("update");
-    }
+
+  setup(props) {
+    const currVariant = computed(() => {
+      switch (props.variant) {
+        case "secondary":
+          return "snackbar__secondary";
+        case "success":
+          return "snackbar__success";
+        default:
+          return "snackbar__primary";
+      }
+    });
+
+    return { currVariant };
   }
 });
 </script>
 
 <style scoped>
 .snackbar {
-  position: absolute;
-  width: 480px;
-  height: 56px;
-  right: 0;
-  top: 1.5rem;
-  color: rgbB(255, 255, 255);
-  background: rgb(55, 87, 134);
-  border-radius: 4px;
+  position: relative;
   display: flex;
+  justify-content: center;
   align-items: center;
-  justify-content: space-between;
+  border-radius: 5px;
+  width: 100%;
+  height: 2.5rem;
 }
 
-.msg {
-  color: rgb(255, 255, 255);
-  padding: 1rem;
+.snackbar__primary {
+  background: var(--color-primary);
+  color: var(--color-card);
 }
 
-/* FIXME: Adjust relative units */
-.close {
+.snackbar__secondary {
+  background: var(--color-card);
+  border: 2px solid var(--color-primary);
+  color: var(--color-primary);
+}
+
+.snackbar__success {
+  background: var(--color-success);
+  color: var(--color-card);
+}
+
+.btn--close {
   position: absolute;
   right: 1.5rem;
-  width: 20px;
-  height: 20px;
-  opacity: 1;
-  cursor: pointer;
   align-self: center;
 }
 
-.close:hover {
+.btn--close:hover {
   opacity: 0.3;
-}
-
-.close:before,
-.close:after {
-  position: absolute;
-  right: 1.5rem;
-
-  content: " ";
-  height: 21px;
-  width: 2px;
-  background-color: RGB(255, 255, 255);
 }
 </style>
