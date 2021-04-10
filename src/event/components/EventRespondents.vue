f79276663058ba4d82eb01ffbff1f6ae39ef1f9c
tnwin <24283779+tnwin@users.noreply.github.com>
2021-04-10 04:28:31 -0700

    refactor: simplified code

--- src/event/components/EventRespondents.vue
+++ src/event/components/EventRespondents.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    <h5>Respondents ({{ respondents.length }})</h5>
+  <div class="respondents--container">
+    <h5 class="respondents--header">Respondents ({{ respondents.length }})</h5>
 
-    <ul class="respondents">
-      <li v-for="(resp, index) in respondents" :key="index" class="resp-item">
-        {{ resp }}
+    <ul class="respondents--list">
+      <li
+        v-for="(respondent, index) in respondents"
+        :key="index"
+        class="respondents--item"
+      >
+        {{ respondent }}
       </li>
     </ul>
   </div>
@@ -15,58 +19,33 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "EventRespondents",
+
   props: {
     respondents: {
       type: Array,
       required: false,
-      default: () => [
-        "Tommy",
-        "James",
-        "Francis",
-        "Jill",
-        "Nanda",
-        "Jacky",
-        "Gokce",
-        "Kai",
-        "Henry",
-        "Tiffany",
-        "Michelle",
-        "Tommy",
-        "James",
-        "Francis",
-        "Jill",
-        "Nanda",
-        "Jacky",
-        "Gokce",
-        "Kai",
-        "Henry",
-        "Tiffany",
-        "Michelle"
-      ]
+      default: () => Array.from(Array(21), (_, index) => `Demo ${++index}`)
     }
   }
 });
 </script>
 
 <style scoped>
-h5 {
-  font-weight: 500;
+.respondents--container {
+  height: 100%;
 }
 
-.respondents {
-  width: 14.375rem;
-  height: 25.3125rem;
-  padding: 1rem 1.125rem;
-  border-radius: 5px;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  background: rgb(255, 255, 255);
-  text-align: left;
-  font-weight: 500;
+.respondents--header {
+  margin-bottom: 1rem;
+}
+
+.respondents--list {
+  height: 100%;
   overflow: hidden;
   overflow-y: auto;
 }
 
-.resp-item {
+.respondents--item {
   margin-bottom: 0.75rem;
 }
 </style>
