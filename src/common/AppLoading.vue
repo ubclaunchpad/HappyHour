0342894a707e0a8e32e6e0f7c290c4df4e7e0e71
tnwin <24283779+tnwin@users.noreply.github.com>
2021-04-10 04:29:39 -0700

    feat: reusable animaed loading component

---
+++ src/common/AppLoading.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="loading">Loading <span>.</span><span>.</span><span>.</span></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AppLoading"
+});
+</script>
+
+<style scoped>
+.loading {
+  font-size: 3rem;
+}
+
+@keyframes blink {
+  0% {
+    opacity: 0.2;
+  }
+
+  20% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0.2;
+  }
+}
+
+.loading span {
+  animation-name: blink;
+  animation-duration: 1.4s;
+  animation-iteration-count: infinite;
+  animation-fill-mode: both;
+}
+
+.loading span:nth-child(2) {
+  animation-delay: 0.2s;
+}
+
+.loading span:nth-child(3) {
+  animation-delay: 0.4s;
+}
+</style>
